cda8be6470c89c5ca21febdc56843c538e6b4654
Marcos Cogo <marcos.cogo@kellogg.northwestern.edu>
2021-04-18 19:02:30 -0500

    hw3

--- noober.js
+++ noober.js
@@ -13,33 +13,46 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
-  <!-- Include the following information on screen -->
+  // Include the following information on screen -->
 
-  <!-- Name
-  Phone Number
-  Pickup Location
-  Dropoff Location
-  Number of passengers in their party
-  Whether or not they would like a luxury "Noober Purple" car -->
+  // Name
+  // Phone Number
+  // Pickup Location
+  // Dropoff Location
+  // Number of passengers in their party
+  // Whether or not they would like a luxury "Noober Purple" car
   
-  <!-- Retrieve the ride details from the Chrome editor, store it in memory -->
+  // Retrieve the ride details from the Chrome editor, store it in memory
+
+    let firstName = ride.passengerDetails.first
+    let lastName = ride.passengerDetails.last
+    let phoneNumber = ride.passengerDetails.phoneNumber
+    let pickupLocation = ride.pickupLocation.address
+    let pickupCity = ride.pickupLocation.city 
+    let pickupState = ride.pickupLocation.state 
+    let pickupZip = ride.pickupLocation.zip 
+    let dropoffLocation = ride.dropoffLocation.address 
+    let dropoffCity = ride.dropoffLocation.city  
+    let dropoffState = ride.dropoffLocation.state 
+    let dropoffZip = ride.dropoffLocation.zip
+    let purpleRequested = ride.purpleRequested
+    let passengerCount = ride.numberOfPassengers
   
-  <div>
-    let FirstName = ride.passengerDetails.first
-    let LastName = ride.passengerDetails.last
-    let PhoneNumber = ride.passengerDetails.phoneNumber
-    let PickupLocation = ride.pickuplocation.address
-    let PickupCity = ride.pickuplocation.city 
-    let PickupState = ride.pickuplocation.state 
-    let PickupZip = ride.pickuplocation.zip 
-    let DropoffLocation = ride.dropofflocation.address 
-    let DropoffCity = ride.dropofflocation.city  
-    let DropoffState = ride.dropofflocation.state 
-    let DropoffZip = ride.dropofflocation.zip 
-  </div>  
-  
-  <!-- Copy ride details to screen -->
-  console.log
+// Use ride details to determine ride type
+
+    let rideType
+      if (ride.purpleRequested == true) {
+        rideType = 'Noober Purple'
+      } else if (ride.numberofpassengers > 3) {
+        rideType = 'Noober XL'
+      } else {
+        rideType = 'Noober X'
+      }
+
+// Copy ride details to screen
+console.log(`Noober X Passenger: ${firstName} ${lastName} - ${phoneNumber}. 
+  Pickup at ${pickupLocation}, ${pickupCity}, ${pickupState} ${pickupZip}. 
+  Drop off ${dropoffLocation}, ${dropoffCity}, ${dropoffState} ${dropoffZip}.`)
 
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
